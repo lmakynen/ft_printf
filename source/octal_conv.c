255fbe95ed03d74bc86403a382fe3e739c3e3c85
Louisa Mäkynen <louisamakynen@Louisa-MacBook-Air-2.local>
2021-04-12 21:34:02 +0300

    fixed #.0

--- source/octal_conv.c
+++ source/octal_conv.c
@@ -6,7 +6,7 @@
 /*   By: lmakynen <lmakynen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 16:21:57 by lmakynen          #+#    #+#             */
-/*   Updated: 2021/04/12 18:42:14 by lmakynen         ###   ########.fr       */
+/*   Updated: 2021/04/12 21:25:09 by lmakynen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@ static void	print_all(t_struct *s, char *str, int len, uintmax_t i)
 	print_value(s, str, i, 2);
 	if (s->width > 0)
 		print_space(s, 2);
+	if (i == 0 && s->hash == 1 && s->precision == -1)
+	{
+		write(1, "0", 1);
+		s->printed++;
+	}
 	s->printed += len;
 }
 
